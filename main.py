a4d91f0c17ead6192602308fa53032ea7f4942b5
Matheus Pacco <68287006+MatheusPacco@users.noreply.github.com>
2022-09-15 12:15:20 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+# print("Informe quantos alimentos você ingeriu hoje!")
+# qtd_alimentos = int(input(""))
+# qtd_calorias_totais = 0;  
+
+# for x in range(1,qtd_alimentos + 1,1):
+#     print()
+    
+#     calorias = int(input("Quantidade Calorias do Alimento"))
+#     qtd_calorias_totais += calorias
+    
+# print("A quantidade de calorias totais foi de: {}".format(qtd_calorias_totais))
+print("----------------------------------------------------------------")
+# Exercício 2 trasições FINANCEIRAS
+
+# print("A quantidade de transições que você realizou durante o dia!")
+# trasicoes = int(input())
+# gastos_totais = 0 
+# media_gastos = 0 
+
+# for x in range(1,trasicoes + 1, 1):
+#     print("Quantos reais você gastou na {}º transição: ".format(x))
+#     gastos = float(input())
+#     gastos_totais += gastos
+
+# # Calculando a média de gastos
+# media_gastos = gastos_totais / trasicoes
+
+# # Apresentando os resultados finais
+# print(f"Você gastou ao todo R${gastos_totais:.2f}")
+# print(f"Sua média de gastos foi de: R${media_gastos}")
+
+# print("----------------------------------------------------------------")
+
+# Exercício 3 - Sequencia de fribonacci!
+
+n1, n2 = 0, 1
+count = 0
+
+while()
+
+
